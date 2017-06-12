869b4cdf847dae3a6c735927a6628db53233e142
Vjeko Brajkovic <vjeko@brajkovic.org>
2017-06-11 19:53:47 -0700

    Migrate to tokio-serial

--- src/insteon_structs.rs
+++ src/insteon_structs.rs
@@ -1,17 +1,12 @@
-use tty::*;
+
+use bytes::BytesMut;
+use bytes::{Buf, IntoBuf};
 
 #[derive(Debug)]
 pub enum InsteonMsg {
 
     StandardMsg {
         addr_from: [u8; 3],
-        addr_to: [u8; 3],
-        msg_flags: u8,
-        cmd1: u8,
-        cmd2: u8,
-    },
-
-    SendStandardMsg {
         addr_to: [u8; 3],
         msg_flags: u8,
         cmd1: u8,
@@ -62,7 +57,15 @@ pub enum InsteonMsg {
 
     AllLinkCleanupStatusReport {
         status_byte: u8,
+    },
+
+    SendStandardMsg {
+        addr_to: [u8; 3],
+        msg_flags: u8,
+        cmd1: u8,
+        cmd2: u8,
     }
+
 }
 
 const STANDARD_MSG :u8 = 0x50;
@@ -74,99 +77,134 @@ const USER_RESET_DETECTED :u8 = 0x55;
 const ALL_LINK_CLEANUP_FAILURE_REPORT :u8 = 0x56;
 const ALL_LINK_RECORD_RESPONSE :u8 = 0x57;
 const ALL_LINK_CLEANUP_STATUS_REPORT :u8 = 0x58;
+const SEND_STANDARD_MSG :u8 = 0x62;
 
 impl InsteonMsg {
-    pub fn new(port: &mut Port) -> InsteonMsg {
-        let command_type :u8 = port.take(1).next().unwrap();
+    pub fn new(buf: &BytesMut) -> Option<(InsteonMsg, usize)> {
 
+        if buf.is_empty() {
+            return None
+        };
+
+        let mut current = buf.into_buf().iter();
+        let command_type = current.next().unwrap();
+        let buf_size = buf.len() - 1;
+
+        //debug!("Received command: {}", command_type);
         match command_type {
             STANDARD_MSG => {
-                let v : Vec<u8> = port.take(9).collect();
+                const MSG_SIZE : usize = 9;
+                let v : Vec<u8> = current.take(MSG_SIZE).collect();
+                if buf_size < MSG_SIZE {
+                    return None
+                }
 
-                InsteonMsg::StandardMsg {
+                Some((InsteonMsg::StandardMsg {
                     addr_from : [v[0], v[1], v[2]],
                     addr_to : [v[3], v[4], v[5]],
                     msg_flags : v[6],
                     cmd1 : v[7],
                     cmd2 : v[8],
-                }
+                }, MSG_SIZE))
             },
 
             EXTENDED_MSG => {
-                let v : Vec<u8> = port.take(23).collect();
+                const MSG_SIZE : usize = 23;
+                let v : Vec<u8> = current.take(MSG_SIZE).collect();
 
-                InsteonMsg::ExtendedMsg {
+                Some((InsteonMsg::ExtendedMsg {
                     addr_from : [v[0], v[1], v[2]],
                     addr_to : [v[3], v[4], v[5]],
                     msg_flags : v[6],
                     cmd1 : v[7],
                     cmd2 : v[8],
                     user_data : [v[9], v[10], v[11], v[12], v[13], v[14], v[15], v[16],
-                                 v[17], v[18], v[19], v[20], v[21], v[22]]
+                        v[17], v[18], v[19], v[20], v[21], v[22]]
 
-                }
+                }, MSG_SIZE))
             },
 
             X10_RECEIVED => {
-                let v : Vec<u8> = port.take(9).collect();
+                const MSG_SIZE : usize = 2;
+                let v : Vec<u8> = current.take(MSG_SIZE).collect();
 
-                InsteonMsg::X10Received{
+                Some((InsteonMsg::X10Received{
                     raw_x10 : v[0],
                     x10_flag : v[1],
-                }
+                }, MSG_SIZE))
             },
 
             ALL_LINKING_COMPLETED => {
-                let v : Vec<u8> = port.take(8).collect();
+                const MSG_SIZE : usize = 8;
+                let v : Vec<u8> = current.take(MSG_SIZE).collect();
 
-                InsteonMsg::AllLinkingCompleted{
+                Some((InsteonMsg::AllLinkingCompleted{
                     link_code: v[0],
                     all_link_group: v[1],
                     id: [v[2], v[3], v[4]],
                     device_category: v[5],
                     device_subcategory: v[6],
                     firmware_version: v[7],
-                }
+                }, MSG_SIZE))
             },
 
 
             BUTTON_EVENT_REPORT => {
-                let v : Vec<u8> = port.take(1).collect();
+                const MSG_SIZE : usize = 1;
+                let v : Vec<u8> = current.take(MSG_SIZE).collect();
 
-                InsteonMsg::ButtonEventReport {
+                Some((InsteonMsg::ButtonEventReport {
                     button_event : v[0],
-                }
+                }, MSG_SIZE))
             },
 
-            USER_RESET_DETECTED => InsteonMsg::UserResetDetected {},
+            USER_RESET_DETECTED => {
+                const MSG_SIZE : usize = 0;
+                Some((InsteonMsg::UserResetDetected {}, MSG_SIZE))
+            },
 
             ALL_LINK_CLEANUP_FAILURE_REPORT => {
-                let v : Vec<u8> = port.take(1).collect();
+                const MSG_SIZE : usize = 1;
+                let v : Vec<u8> = current.take(MSG_SIZE).collect();
 
-                InsteonMsg::AllLinkCleanupFailureReport {
+                Some((InsteonMsg::AllLinkCleanupFailureReport {
                     x01: v[0],
                     all_link_group: v[1],
                     id: [v[2], v[3], v[4]],
-                }
+                }, MSG_SIZE))
             },
 
             ALL_LINK_RECORD_RESPONSE => {
-                let v: Vec<u8> = port.take(8).collect();
+                const MSG_SIZE : usize = 8;
+                let v: Vec<u8> = current.take(MSG_SIZE).collect();
 
-                InsteonMsg::AllLinkRecordResponse {
+                Some((InsteonMsg::AllLinkRecordResponse {
                     all_link_record_flags: v[0],
                     all_link_group: v[1],
                     id: [v[2], v[3], v[4]],
                     link_data: [v[5], v[6], v[7]],
-                }
+                }, MSG_SIZE))
             },
 
             ALL_LINK_CLEANUP_STATUS_REPORT => {
-                let v : Vec<u8> = port.take(1).collect();
+                const MSG_SIZE : usize = 1;
+                let v : Vec<u8> = current.take(MSG_SIZE).collect();
 
-                InsteonMsg::AllLinkCleanupStatusReport {
-                     status_byte : v[0],
-                }
+                Some((InsteonMsg::AllLinkCleanupStatusReport {
+                    status_byte : v[0],
+                }, MSG_SIZE))
+            },
+
+            SEND_STANDARD_MSG => {
+                const MSG_SIZE : usize = 6;
+                let v : Vec<u8> = current.take(6).collect();
+
+                Some((InsteonMsg::SendStandardMsg {
+                    addr_to : [v[0], v[1], v[2]],
+                    msg_flags : v[3],
+                    cmd1: v[4],
+                    cmd2: v[5]
+                }, MSG_SIZE))
             },
 
             _ => panic!("unknown command type: {:?}", command_type),
--- src/main.rs
+++ src/main.rs
@@ -2,48 +2,107 @@
 #![feature(mpsc_select)]
 
 pub mod insteon_structs;
-pub mod tty;
 
-use insteon_structs::*;
-use tty::*;
+extern crate futures;
+extern crate tokio_serial;
+extern crate tokio_core;
+extern crate tokio_io;
+extern crate bytes;
+
+use std::{io, str};
 use std::{thread, time};
 use std::sync::mpsc;
+use std::time::Duration;
 
-fn receive_cmd(port: &mut Port) -> InsteonMsg {
-    const COMMAND_START :u8 = 0x02;
-    port.skip_while(|x| x != &COMMAND_START).next();
-    return InsteonMsg::new(port)
+use tokio_core::reactor::Core;
+use tokio_io::codec::{Decoder, Encoder};
+use tokio_io::AsyncRead;
+use tokio_serial::*;
+
+use bytes::BytesMut;
+
+use futures::stream::Stream;
+use futures::stream::SplitSink;
+use futures::Sink;
+use futures::sink::Send;
+use futures::Future;
+
+use insteon_structs::*;
+
+pub struct LineCodec;
+
+impl Decoder for LineCodec {
+    type Item = InsteonMsg;
+    type Error = io::Error;
+
+    fn decode(&mut self, src: &mut BytesMut) -> Result<Option<Self::Item>, Self::Error> {
+        const COMMAND_START :u8 = 0x02;
+        match src.iter().position(|x| *x == COMMAND_START ) {
+            Some(idx) => src.split_to(idx + 1),
+            None => {
+                src.clear();
+                return Ok(None);
+            }
+        };
+
+        match InsteonMsg::new(&src) {
+            Some((msg, msg_size)) => {
+                src.split_to(msg_size);
+                Ok(Some(msg))
+            },
+            None => Ok(None)
+        }
+
+    }
 }
 
+impl Encoder for LineCodec {
+    type Item = Vec<u8>;
+    type Error = io::Error;
+
+    fn encode(&mut self, _item: Self::Item, _dst: &mut BytesMut) -> Result<(), Self::Error> {
+        _dst.extend_from_slice(&_item);
+        Ok(())
+    }
+}
+
+
 fn main() {
-    let port_path = String::from("/dev/ttyUSB0");
-    let mut port = Port::new(port_path);
 
-    let (tx_main, rx_main) = mpsc::channel();
+    let mut core = Core::new().unwrap();
+    let handle = core.handle();
 
-    thread::spawn(move || {
-        loop {
-            let msg = receive_cmd(&mut port);
-            tx_main.send(msg).unwrap();
-        }
+    let tty_path = "/dev/ttyUSB0";
+    let settings = SerialPortSettings {
+        baud_rate: BaudRate::Baud19200,
+        data_bits: DataBits::Eight,
+        flow_control: FlowControl::None,
+        parity: Parity::None,
+        stop_bits: StopBits::One,
+        timeout: Duration::from_millis(1),
+    };
+
+
+    let mut port = tokio_serial::Serial::from_path(tty_path, &settings, &handle).unwrap();
+    port.set_exclusive(false).expect("Unable to set serial port exclusive");
+
+    let (mut writer, reader) = port.framed(LineCodec).split();
+
+    let printer = reader.for_each(|s| {
+        println!("CMD: {:?}", s);
+
+        std::thread::sleep(time::Duration::from_secs(2));
+        let on = vec![0x02, 0x62, 65, 29, 30, 64, 17, 1];
+        writer.start_send(on);
+        writer.poll_complete();
+
+        std::thread::sleep(time::Duration::from_secs(2));
+        let off = vec![0x02, 0x62, 65, 29, 30, 64, 19, 1];
+        writer.start_send(off);
+        writer.poll_complete();
+
+        Ok(())
     });
 
-    thread::spawn(move || {
-        loop {
-            std::thread::sleep(time::Duration::from_secs(2));
-            let on = vec![0x02, 0x62, 65, 29, 30, 64, 17, 1];
-            //port.send(&on);
-            std::thread::sleep(time::Duration::from_secs(2));
-            let off = vec![0x02, 0x62, 65, 29, 30, 64, 19, 1];;
-            //port.send(&off);
-
-            println!("==================");
-        }
-    });
-
-    thread::spawn(move || {
-        loop {
-            println!("msg {:?}", rx_main.recv().unwrap());
-        }
-    }).join().unwrap();
+    core.run(printer).unwrap();
 }
